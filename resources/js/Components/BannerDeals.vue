156be7c9d0e6c842c119ef3390ec01981dceb816
mthiagoalves <m.thiago.ales@live.com>
2024-02-21 19:10:58 +0000

    feat(bannerDeals): implement banner deals

--- resources/js/Components/BannerDeals.vue
+++ resources/js/Components/BannerDeals.vue
@@ -1,31 +1,53 @@
 <template>
-    <swiper :modules="modules" :slides-per-view="1" effect="fade" :space-between="8" navigation :pagination="{ clickable: true }"
-        @swiper="onSwiper" @slideChange="onSlideChange">
+    <swiper :modules="modules" :slides-per-view="1" :space-between="8" navigation
+        :pagination="{ clickable: true, dynamicBullets: true }" :loop="true" :loop-additional-slides="6" @swiper="onSwiper"
+        @slideChange="onSlideChange" :autoplay="{ delay: 2500, disableOnInteraction: false }">
         <swiper-slide>
-            <img src="https://dummyimage.com/1800x600/bd7daf/fff" alt="">
+            <img src="/imgs/banners/banner-01.png" alt="" class="hidden sm:block">
+            <img src="/imgs/banners/banner-01-m.png" alt="" class="block sm:hidden">
         </swiper-slide>
         <swiper-slide>
-            <img src="https://dummyimage.com/1800x600/bd7f9e/fff" alt="">
+            <img src="/imgs/banners/banner-02.png" alt="" class="hidden sm:block">
+            <img src="/imgs/banners/banner-02-m.png" alt="" class="block sm:hidden">
         </swiper-slide>
         <swiper-slide>
-            <img src="https://dummyimage.com/1800x600/d45594/fff" alt="">
+            <img src="/imgs/banners/banner-03.png" alt="" class="hidden sm:block">
+            <img src="/imgs/banners/banner-03-m.png" alt="" class="block sm:hidden">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="/imgs/banners/banner-04.png" alt="" class="hidden sm:block">
+            <img src="/imgs/banners/banner-04-m.png" alt="" class="block sm:hidden">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="/imgs/banners/banner-05.png" alt="" class="hidden sm:block">
+            <img src="/imgs/banners/banner-05-m.png" alt="" class="block sm:hidden">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="/imgs/banners/banner-06.png" alt="" class="hidden sm:block">
+            <img src="/imgs/banners/banner-06-m.png" alt="" class="block sm:hidden">
         </swiper-slide>
     </swiper>
 </template>
 
+<style>
+.swiper-button-prev,
+.swiper-button-next {
+    color: var(--color-standard);
+}
+
+.swiper-pagination-bullet-active {
+    background-color: var(--color-standard);
+}
+</style>
+
 <script>
-// import Swiper core and required modules
-import { Navigation, Pagination, A11y, EffectFade } from 'swiper/modules';
-
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/vue';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-// Import Swiper styles
 export default {
     components: {
         Swiper,
@@ -39,10 +61,11 @@ export default {
         const onSlideChange = () => {
             console.log('slide change');
         };
+
         return {
             onSwiper,
             onSlideChange,
-            modules: [Navigation, Pagination, A11y, EffectFade],
+            modules: [Navigation, Pagination, A11y, Autoplay],
         };
     },
 };
