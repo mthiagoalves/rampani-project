e85c523cc29ba66d4dbf0ebb41fe6aa35e32c6a8
mthiagoalves <m.thiago.ales@live.com>
2024-03-02 16:01:05 +0000

    docs(app): add new CDN sweet alert

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,27 +1,33 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-        <meta name="title" property="og:title" content="Clinica Rampani">
-        <meta name="description" property="og:description" content="meta decription here">
-        <meta name="image" property="og:image" content="/imgs/icons/title-icon.png">
-        <link rel="icon" href="/imgs/icons/title-icon.png">
-        <link href="./css/style.css" rel="stylesheet">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <meta name="title" property="og:title" content="Clinica Rampani">
+    <meta name="description" property="og:description" content="meta decription here">
+    <meta name="image" property="og:image" content="/imgs/icons/title-icon.png">
+    <link rel="icon" href="/imgs/icons/title-icon.png">
+    <link href="./css/style.css" rel="stylesheet">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.min.css">
+
+    <!-- Scripts -->
+    @routes
+    @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.all.min.js"></script>
+</body>
+
 </html>
