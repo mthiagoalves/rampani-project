86d785977f62eda2cd79369f88f1399ee44acf5f
mthiagoalves <m.thiago.ales@live.com>
2024-03-02 15:54:26 +0000

    feat(FormsController): create forms controller

---
+++ app/Http/Controllers/FormsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormsController extends Controller
+{
+    public function geralScheduling(Request $request)
+    {
+
+        return response()->json(["hahaha"]);
+    }
+}
