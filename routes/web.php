d6bb8acd31bc25b723a418f9746c87544d6c35ef
mthiagoalves <m.thiago.ales@live.com>
2024-03-02 16:00:21 +0000

    feat(web.php): add news routes and change process to controller

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,23 @@
 <?php
 
+use App\Http\Controllers\FormsController;
+use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Homepage');
-})->name('homepage');
+Route::get('/', [FrontendController::class, 'getHomepage'])->name('homepage');
+
+Route::get('/procedimentos', [FrontendController::class, 'getProcedures'])->name('procedures');
+
+Route::get('/termos-e-condicoes', [FrontendController::class, 'getTermsAndConditions'])->name('terms-and-conditions');
+
+Route::get('/politica-de-privacidade', [FrontendController::class, 'getPrivacyPolicy'])->name('privacy-policy');
+
+// Forms Process
+
+Route::post('/geral-scheduling', [FormsController::class, 'geralScheduling'])->name('geralScheduling');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
