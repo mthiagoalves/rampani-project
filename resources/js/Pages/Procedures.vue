2f8742f56fa0b131945604770c462ff92e485a13
mthiagoalves <m.thiago.ales@live.com>
2024-03-02 15:59:51 +0000

    feat(Procedures): create new page procedures

---
+++ resources/js/Pages/Procedures.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+import Procedures from '@/Components/Procedures/Procedures.vue';
+
+
+</script>
+<template>
+    <MainLayout>
+       <Procedures></Procedures>
+    </MainLayout>
+</template>
